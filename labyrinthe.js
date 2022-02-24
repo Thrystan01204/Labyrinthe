3bb7c571993e11aba188712525ee763bb8d85ac9
Pierre Jaffuer <p.jaffuer@free.fr>
2022-02-24 23:23:21 +0400

    cell walls

--- labyrinthe.js
+++ labyrinthe.js
@@ -1,33 +1,36 @@
-var body;
+var mouseDown = 0;
 
 function onLoad() {
     body = document.body;
+    document.body.onmousedown = function() { 
+        mouseDown = true;
+      }
+      document.body.onmouseup = function() {
+        mouseDown = false;
+      }
     
-
-    var n = 25;
+    var n = 30;
     var cellSize = Math.min(window.innerWidth, window.innerHeight)/n; 
-
     var maze = document.createElement("div");
     maze.classList.add("maze");
-    //maze.style.width = cellSize*n+"px";
-    //maze.style.height = cellSize*n+"px";
-    //maze.style.gridTemplateColumns = "repeat("+n+","+cellSize+"px)";
-    //maze.style.gridTemplateRows = "repeat("+n+","+cellSize+"px)";
     maze.style.gridTemplateColumns = "repeat("+n+",1fr)";
     maze.style.gridTemplateRows = "repeat("+n+",1fr)";
-
     var cells = [];
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             let cell = document.createElement("div");
             cell.classList.add("cell");
-            cell.onclick = () => {
-                cell.style.backgroundColor="grey";
+            cell.onmouseenter = (event) => {
+                if(mouseDown){
+                    cell.style.border = "0";
+                }
+            };
+            cell.onmousedown = (event) => {
+                cell.style.border = "0";
             };
             maze.appendChild(cell);
         }    
     }
-
     body.appendChild(maze);
 
 }
